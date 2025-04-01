1b75ae58da55a35539bfb4800aa83f7e40ce96c7
ANDRE LUIZ PINHEIRO DA SILVA <andre.lpsilva7@senacsp.edu.br>
2025-03-31 22:28:17 -0300

    cosas

---
+++ BancoDados/André_Luiz_Pinheiro.sql
@@ -0,0 +1,177 @@
+/*//TABELA EMPREGADOS
+*/
+CREATE TABLE IF NOT EXISTS empregado (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    idade INT(3) NOT NULL,
+    id_departamento INT NOT NULL,
+    salario DECIMAL NOT NULL,
+    FOREIGN KEY (id_departamento)
+        REFERENCES departamento (id)
+);
+INSERT INTO empregado
+(
+ nome, idade, id_departamento, salario
+)
+VALUES
+('João', '30', '1', '50000'),
+('Sarah', '28','2','60000'),
+('Miguel','35','3','55000'),
+('Ana','27','2','62000'),
+('Tomás','42','3','58000');
+SELECT * FROM empregado;
+
+
+/*//TABELA DEPARTAMENTO
+*/
+CREATE TABLE IF NOT EXISTS departamento
+(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR (50) NOT NULL UNIQUE
+);
+INSERT INTO departamento
+(
+ nome
+)
+VALUE
+('RH'),
+('TI'),
+('Vendas'),
+('Marketing'),
+('Segurança');
+
+SELECT * FROM departamento;
+
+drop table empregado;
+drop table departamento;
+
+
+
+/*//EXERCICIO 1
+*/
+-- visão geral da tabela de empregados
+SELECT 
+    *
+FROM
+    empregado;
+    
+-- visualizar apenas os empregados do departamneto de TI
+SELECT 
+    *
+FROM
+    empregado
+WHERE
+    departamento = 'TI';
+    
+-- empregados que ganham mais de 55000
+    SELECT 
+    empregado.nome, empregado.salario
+FROM
+    empregado
+WHERE
+    salario > '55000';
+    
+-- lisat de empregados em ordem crescente
+SELECT 
+    *
+FROM
+    empregado
+ORDER BY idade ASC;
+
+/*//EXERCICIO 2
+*/
+-- selecionar empregados com idade entre 28 e 35 anos.,
+    SELECT 
+    *
+FROM
+    empregado
+WHERE
+    idade > '27' AND idade < '36';
+
+-- NOMES INICIADOS EM 'M'
+SELECT 
+    *
+FROM
+    empregado
+WHERE
+    nome LIKE 'M%';
+
+-- EMPREGADOS QUE NÃO ESTÃO NO DEPARTAMENTO 'RH' 
+SELECT 
+    *
+FROM
+    empregado
+WHERE
+    departamento != 'RH';
+
+/*//EXERCICIO 3
+
+*/
+-- SELECIONE O NUMERO DE EMPREGADOS POR DEPARTAMENTO
+SELECT COUNT(id) FROM empregado WHERE departamento = 'RH';
+
+SELECT COUNT(id) FROM empregado WHERE departamento = 'TI';
+
+SELECT COUNT(id) FROM empregado WHERE departamento = 'VENDAS';
+
+-- MÉDIA SALARIAL
+SELECT AVG(salario) FROM empregado;
+
+-- MÉDIA SALARIAL TI
+SELECT AVG(salario) FROM empregado WHERE departamento = 'TI';
+
+-- TOTAL SALARIOS VENDAS
+SELECT SUM(salario) FROM empregado WHERE departamento = 'Vendas';
+
+/*//EXERCICIO 4
+*/
+-- EMPREGADOS COM SEUS RESPECTIVOS DEPARTAMENTOS
+SELECT 
+    empregado.id,
+    empregado.nome,
+    empregado.salario,
+    departamento.nome AS departamento
+FROM
+    empregado
+        INNER JOIN
+    departamento ON empregado.id_departamento = departamento.id;
+
+-- TODOS OS DEPARTAMENTOS, MESMO QUE SEM EMPREGADOS
+SELECT 
+    empregado.id,
+    empregado.nome,
+    empregado.salario,
+    departamento.nome AS departamento
+FROM
+    departamento
+        LEFT JOIN
+    empregado ON departamento.id = empregado.id_departamento;
+    
+/*//EXERCICIO 5
+
+*/
+--
+SELECT 
+    *
+FROM
+    empregado
+WHERE
+    salario > (SELECT 
+            AVG(salario)
+        FROM
+            empregado);
+
+--
+SELECT id_departamento FROM empregado WHERE nome = 'Sarah';
+
+/*//EXERCICIO 6
+*/
+
+SELECT 
+   *
+FROM
+    empregado
+WHERE
+    id_departamento = 'TI'; 
+
+
--- BancoDados/table_anotações.sql
+++ BancoDados/table_anotações.sql
@@ -1,3 +1,7 @@
+/*
+//
+//FORMAS DIFERENTES DE VERIFICAR UMA DATA.
+//
 SELECT 
     *
 FROM
@@ -17,15 +21,82 @@ FROM
     livro
 WHERE year(data_publicacao) between '1990' AND '1999';
 
+//
+//COMO PROCURA POR UM DADO SEM REPETILO QUANDO APARECE MAIS DE UMA VEZ.
+//
 select distinct(autor) from livro;
 
+//
+//METODOS PARA VERIFICAR VALORES OU CONTAS ARITMÉTICAS.
+//
 select sum(preco),avg(preco),max(preco),min(preco)  from livro;
 
 select count(id) from livro;
 
+//
+//ORDENAR UMA COLUNA POR ORDEM CRESCENTE E DESCRECENTE.
+//
 select * from livro
 order by titulo, preco asc;
 
 select * from livro
 order by titulo desc;
+
+//
+//PEDIR UMA INFORMAÇÃO MODIFICADA.
+//
+SELECT DISTINCT
+    lower(autor)
+FROM
+    livro; 
+    
+//
+//FORMAS DIFERNETES DE ARMONIZAR UMA BUSCA EM TABELAS 
+//
+//
+//PERFORMATICA
+//
+SELECT 
+    livro.id,
+    livro.titulo,
+    livro.data_publicacao,
+    livro.numero_paginas,
+    livro.preco,
+    livro.isbn,
+    genero.nome AS genero,
+    autor.nome AS autor,
+    editora.nome AS editora
+    FROM 
+    livro
+    INNER JOIN genero ON livro.id_genero = genero.id
+    INNER JOIN autor ON livro.id_autor = autor.id
+    INNER JOIN editora ON livro.id_editora = editora.id;
+//
+//CUSTOSA
+//
+SELECT 
+    livro.id,
+    livro.titulo,
+    livro.data_publicacao,
+    livro.numero_paginas,
+    livro.preco,
+    livro.isbn,
+    genero.nome AS genero,
+    autor.nome AS autor,
+    editora.nome AS editora
+    
+FROM
+	livro,
+    genero,
+    autor,
+    editora
+WHERE 
+	livro.id_genero = genero.id
+    and
+    livro.id_autor = autor.id
+    and
+    livro.id_editora = editora.id
+
+*/
+
 
