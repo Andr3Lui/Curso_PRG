e720d25ea763e451afe072c6128d1bf18654a357
ANDRE LUIZ PINHEIRO DA SILVA <andre.lpsilva7@senacsp.edu.br>
2025-04-14 22:27:46 -0300

    14/04/2025

---
+++ ListaAtividades/Banco_Dados/DataBase.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ListaAtividades.Banco_Dados
+{
+    internal class DataBase
+    {
+        private static readonly string ConnectionString = "datasource=localhost;username=root;password=;database=senac;";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+    }
+}
---
+++ ListaAtividades/Dominio/Atividade.cs
@@ -0,0 +1,44 @@
+﻿
+
+using ListaAtividades.AtividadeRepositorio;
+
+
+namespace ListaAtividades.Dominio
+{
+    internal class Atividade
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public Situacao Situacao { get; set; }
+
+        public void Criar()
+        {
+
+        }
+
+        public void AtualizarSituacao()
+        {
+
+        }
+
+        public Atividade BuscarAtividadeEmAndamento()
+        {
+            return new Atividade();
+        }
+
+        public List<Atividade> ListarAtividadePendentes()
+        {
+            return [];
+        }
+
+        private bool ValidarTitulo()
+        {
+            return !string.IsNullOrEmpty(Titulo);
+        }
+
+        private Situacao BuscarProximoSituacao()
+        {
+            return Situacao.Concluido;
+        }
+    }
+}   
